34e59f01eaa4ba7550c9f2575f19910edf52e4f1
Will Barnes <will.t.barnes@gmail.com>
2023-10-03 16:08:27 -0400

    convenience functions for calculating point source spectra

--- mocksipipeline/detector/component.py
+++ mocksipipeline/detector/component.py
@@ -2,13 +2,18 @@
 Module for projecting emission to detector plane
 """
 import astropy.units as u
+from astropy.convolution import convolve, Gaussian1DKernel
 from astropy.wcs.utils import wcs_to_celestial_frame, pixel_to_pixel
+from astropy.stats import gaussian_fwhm_to_sigma
 import dask.array
 import ndcube
+from ndcube.extra_coords import QuantityTableCoordinate
 import numpy as np
 from overlappy.reproject import reproject_to_overlappogram
 from overlappy.util import strided_array
+import tqdm
 
+from mocksipipeline.physics.spectral import calculate_intensity
 from mocksipipeline.detector.response import (convolve_with_response,
                                               get_all_dispersed_channels,
                                               get_all_filtergram_channels)
@@ -18,6 +23,8 @@ __all__ = [
     'DispersedComponent',
     'FiltergramComponent',
     'sample_and_remap_spectral_cube',
+    'dem_table_to_ndcube',
+    'compute_flux_point_source',
 ]
 
 
@@ -95,7 +102,13 @@ class FiltergramComponent:
         return results
 
 
-def sample_and_remap_spectral_cube(spec_cube, channel, dt=1*u.s, interval=20*u.s, chunks=None, **wcs_kwargs):
+def sample_and_remap_spectral_cube(spec_cube,
+                                   channel,
+                                   dt=1*u.s,
+                                   interval=20*u.s,
+                                   convert_to_dn=False,
+                                   chunks=None,
+                                   **wcs_kwargs):
     """
     Map spectral cube to detector plane via Poisson distribution
 
@@ -105,6 +118,9 @@ def sample_and_remap_spectral_cube(spec_cube, channel, dt=1*u.s, interval=20*u.s
     channel
     dt
     interval
+    convert_to_dn: `bool`, optional
+        If True, sum counts in DN. Poisson sampling will still be done
+        in photon space
     """
     # Convert to instrument units
     observer = wcs_to_celestial_frame(spec_cube.wcs).observer
@@ -115,10 +131,20 @@ def sample_and_remap_spectral_cube(spec_cube, channel, dt=1*u.s, interval=20*u.s
         chunks = lam.shape
     lam = dask.array.from_array(lam, chunks=chunks)
     num_iterations = int(np.ceil((interval / dt).decompose()))
+    # NOTE: For a large number of iterations, this can cause strange behavior because of the
+    # very large size of the array
     samples = dask.array.random.poisson(lam=lam, size=(num_iterations,)+lam.shape).sum(axis=0)
     samples = samples.compute()
     idx_nonzero = np.where(samples > 0)
     weights = samples[idx_nonzero]
+    # (Optionally) Convert to DN
+    unit = 'photon'
+    if convert_to_dn:
+        # NOTE: we can select the relevant conversion factors this way because the wavelength
+        # axis of lam is the same as channel.wavelength and thus their indices are aligned
+        ct_per_photon = channel.electron_per_photon * channel.camera_gain
+        weights = weights * ct_per_photon.to_value('ct / ph')[idx_nonzero[0]]
+        unit = 'ct'
     # Map counts to detector coordinates
     overlap_wcs = channel.get_wcs(observer, **wcs_kwargs)
     idx_nonzero_overlap = pixel_to_pixel(instr_cube.wcs, overlap_wcs, *idx_nonzero[::-1])
@@ -130,4 +156,66 @@ def sample_and_remap_spectral_cube(spec_cube, channel, dt=1*u.s, interval=20*u.s
                                 weights=weights)
     return ndcube.NDCube(strided_array(hist, channel.wavelength.shape[0]),
                          wcs=overlap_wcs,
-                         unit='photon')
+                         unit=unit)
+
+
+def compute_flux_point_source(em, spectral_table, location, blur=None, **kwargs):
+    """
+    Compute flux for each channel from a point source.
+    """
+    electrons = kwargs.pop('electrons', False)
+    include_gain = kwargs.pop('include_gain', False)
+    channels = get_all_dispersed_channels()[5:]
+    intensity = calculate_intensity(em, spectral_table, {})
+    pix_grid, _, _ = channels[0].get_wcs(location.observer, **kwargs).world_to_pixel(location, channels[0].wavelength)
+    flux_total = np.zeros(pix_grid.shape)
+    cube_list = []
+    for channel in channels:
+        flux = convolve_with_response(intensity, channel, electrons=electrons, include_gain=include_gain)
+        flux.meta['spectral_order'] = channel.spectral_order
+        _pix_grid, _, _ = channel.get_wcs(location.observer, **kwargs).world_to_pixel(location, channel.wavelength)
+        flux_total += np.interp(pix_grid, _pix_grid, flux.data)
+        if blur:
+            flux = ndcube.NDCube(blur_spectra(flux.data, blur, channel), wcs=flux.wcs, meta=flux.meta, unit=flux.unit)
+        cube_list.append((f'order_{channel.spectral_order}', flux))
+
+    flux_total = ndcube.NDCube(flux_total, wcs=cube_list[0][1].wcs, unit=flux.unit)
+    if blur:
+        flux_total = blur_spectra(flux_total, blur, channels[0])
+    cube_list.append(('total', flux_total))
+
+    return ndcube.NDCollection(cube_list)
+
+
+def blur_spectra(flux, blur, channel):
+    """
+    Blur spectral resolution of flux
+
+    Parameters
+    ----------
+    flux:
+    blur: `~astropy.units.Quantity`
+        FWHM in spectral space of the estimated blur
+    """
+    std = blur / np.fabs(channel.spectral_order) * gaussian_fwhm_to_sigma
+    std_eff = (std / channel.spectral_resolution).to_value('pix')
+    kernel = Gaussian1DKernel(std_eff)
+    return convolve(flux.data, kernel)
+
+
+def dem_table_to_ndcube(dem_table):
+    """
+    Parse an astropy table of DEM information into an NDCube
+
+    Args:
+        dem_table (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    temperature = dem_table['temperature_bin_center']
+    em = dem_table['dem']*np.gradient(temperature, edge_order=2)
+    tab_coord = QuantityTableCoordinate(temperature,
+                                        names='temperature',
+                                        physical_types='phys.temperature')
+    return ndcube.NDCube(em, wcs=tab_coord.wcs, meta=dem_table.meta)
