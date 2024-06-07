c92a407556e831bac768f483e1ba4b6c96b78cf5
Will Barnes <will.t.barnes@gmail.com>
2024-06-07 01:51:17 -0400

    use new psf approach in pipeline

--- pipeline/workflow/scripts/project_intensity_cubes.py
+++ pipeline/workflow/scripts/project_intensity_cubes.py
@@ -1,6 +1,4 @@
 import astropy.units as u
-import scipy.ndimage
-from astropy.stats import gaussian_fwhm_to_sigma
 from astropy.wcs.utils import wcs_to_celestial_frame
 from overlappy.io import write_overlappogram
 
@@ -9,30 +7,20 @@ from mocksipipeline.modeling import (convolve_with_response,
                                      project_spectral_cube)
 from mocksipipeline.util import read_data_cube
 
-
-def apply_psf(cube, channel):
-    # FIXME: This is just a placeholder PSF calculation. Need to implement an actual
-    # convolution with a model PSF that more accurately captures the effect of the slot
-    # on the spatial resolution
-    # NOTE: By applying the PSF on the spectral cube, we are assuming that the pixel grid
-    # here is aligned with the pixel grid of the detector. The PSF FWHM has been carefully
-    # constructed to account for this. In general, we should figure out a way to define
-    # the PSF kernel on the zeroth order detector grid and transform it to this coordinate
-    # system.
-    psf_sigma = channel.aperture.psf_fwhm * gaussian_fwhm_to_sigma / channel.spatial_plate_scale
-    _ = scipy.ndimage.gaussian_filter(cube.data,
-                                      psf_sigma.to_value('pixel')[::-1],
-                                      axes=(1,2),
-                                      output=cube.data)
-    return cube
-
-
 if __name__ == '__main__':
     # Read in config options
     dt = float(snakemake.config['exposure_time']) * u.s
     interval = float(snakemake.config['cadence']) * u.s
     apply_electron_conversion = bool(snakemake.config['apply_electron_conversion'])
     apply_gain_conversion = bool(snakemake.config['apply_gain_conversion'])
+    include_psf = bool(snakemake.config['include_psf'])
+    include_charge_spreading = bool(snakemake.config['include_charge_spreading'])
+    if pointing_jitter := snakemake.config.get('pointing_jitter', None):
+        pointing_jitter = float(pointing_jitter) * u.arcsec
+    # Connect to scheduler if specified
+    if client_address := snakemake.config.get('scheduler_address', None):
+        import distributed
+        client = distributed.Client(address=client_address)
     # Load instrument configuration
     instrument_design = getattr(mocksipipeline.instrument.configuration, snakemake.config['instrument_design'])
     # Select channel
@@ -47,6 +35,9 @@ if __name__ == '__main__':
                                      wcs_to_celestial_frame(spec_cube.wcs).observer,
                                      dt=dt,
                                      interval=interval,
+                                     pointing_jitter=pointing_jitter,
+                                     include_psf=include_psf,
+                                     include_charge_spreading=include_charge_spreading,
                                      apply_gain_conversion=apply_gain_conversion,
                                      apply_electron_conversion=apply_electron_conversion)
     # Save to file
