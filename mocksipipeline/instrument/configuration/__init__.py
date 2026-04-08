bc60d3bf8ce2b710ded9288445faf60a6ab47b1c
Will Barnes <will.t.barnes@gmail.com>
2026-04-08 02:06:17 -0400

    Separate built and as-designed configurations (#47)
    * separate built and as-designed configurations
    
    * xrt responses now have correct units
    
    * move everything to pyproject, bump python version
    
    * fix coverage config
    
    * bump windows down to py13
    
    * remove windows test for now

--- mocksipipeline/instrument/configuration/__init__.py
+++ mocksipipeline/instrument/configuration/__init__.py
@@ -1,3 +1,4 @@
+from .moxsi_built import *
 from .moxsi_cdr import *
 from .moxsi_short import *
 from .moxsi_slot import *
--- mocksipipeline/instrument/configuration/moxsi_built.py
+++ mocksipipeline/instrument/configuration/moxsi_built.py
@@ -1,5 +1,5 @@
 """
-Configurations for MOXSI as of CDR
+Configuration for MOXSI as built
 """
 import astropy.units as u
 import numpy as np
@@ -7,18 +7,28 @@ import numpy as np
 from mocksipipeline.instrument.design import InstrumentDesign
 from mocksipipeline.instrument.optics.aperture import (CircularAperture,
                                                        SlotAperture)
-from mocksipipeline.instrument.optics.configuration import \
-    short_as_designed_design
+from mocksipipeline.instrument.optics.design import OpticalDesign
 from mocksipipeline.instrument.optics.filter import ThinFilmFilter
 from mocksipipeline.instrument.optics.response import (ALLOWED_SPECTRAL_ORDERS,
                                                        Channel)
 
 __all__ = [
-    'moxsi_cdr',
-    'moxsi_cdr_filtergrams',
-    'moxsi_cdr_spectrogram_pinhole',
-    'moxsi_cdr_spectrogram_slot',
+    'moxsi_built',
+    'moxsi_built_filtergrams',
+    'moxsi_built_spectrogram_pinhole',
+    'moxsi_built_spectrogram_slot',
 ]
+
+
+# Optics
+# NOTE: Roll and ruling will be iterated on based on measurements
+short_as_built_design = OpticalDesign(
+    'short_tube_as-built',
+    focal_length=20.8*u.cm,
+    grating_focal_length=19.5*u.cm,
+    grating_groove_spacing=1/5000*u.mm,
+    grating_roll_angle=0*u.deg,
+)
 
 # Build needed filters
 table = 'Chantler'
@@ -40,43 +50,43 @@ slot = SlotAperture(diameter=circular.diameter,
                     center_to_center_distance=9*np.pi*circular.diameter/4)
 
 # Set up reference pixels
-detector_center = np.array(short_as_designed_design.detector_shape)[::-1] / 2
+detector_center = np.array(short_as_built_design.detector_shape)[::-1] / 2
 # Values pulled from mechanical drawing 1/11/24
 filtergram_y = 3.51 * u.mm
 filtergram_x = (np.arange(4) * 3.32 - 1.5 * 3.32) * u.mm
 slot_y = 3.43 * u.mm
 filtergram_ref_pix = [
-    tuple(np.array([x / short_as_designed_design.pixel_size_x,
-                    filtergram_y / short_as_designed_design.pixel_size_y]) + detector_center)
+    tuple(np.array([x / short_as_built_design.pixel_size_x,
+                    filtergram_y / short_as_built_design.pixel_size_y]) + detector_center)
     for x in filtergram_x
 ]
 pinhole_ref_pix = tuple(detector_center)
-slot_ref_pix = tuple(detector_center - [0, slot_y / short_as_designed_design.pixel_size_y])
+slot_ref_pix = tuple(detector_center - [0, slot_y / short_as_built_design.pixel_size_y])
 
 # Set up filtergrams
 filtergram_1 = Channel(name='filtergram_1',
                        order=0,
                        filters=be_thin,
                        reference_pixel=(filtergram_ref_pix[0]+(0,))*u.pix,
-                       design=short_as_designed_design,
+                       design=short_as_built_design,
                        aperture=circular)
 filtergram_2 = Channel(name='filtergram_2',
                        order=0,
                        filters=be_med,
                        reference_pixel=(filtergram_ref_pix[1]+(0,))*u.pix,
-                       design=short_as_designed_design,
+                       design=short_as_built_design,
                        aperture=circular)
 filtergram_3 = Channel(name='filtergram_3',
                        order=0,
                        filters=be_thick,
                        reference_pixel=(filtergram_ref_pix[2]+(0,))*u.pix,
-                       design=short_as_designed_design,
+                       design=short_as_built_design,
                        aperture=circular)
 filtergram_4 = Channel(name='filtergram_4',
                        order=0,
                        filters=[al_oxide, al_thin, polymide],
                        reference_pixel=(filtergram_ref_pix[3]+(0,))*u.pix,
-                       design=short_as_designed_design,
+                       design=short_as_built_design,
                        aperture=circular)
 
 # Set up spectrograms
@@ -88,7 +98,7 @@ for order in ALLOWED_SPECTRAL_ORDERS:
                 order=order,
                 filters=[al_thin, al_oxide],
                 reference_pixel=(pinhole_ref_pix+(0,))*u.pix,
-                design=short_as_designed_design,
+                design=short_as_built_design,
                 aperture=circular)
     )
     spectrograms_slot.append(
@@ -96,15 +106,15 @@ for order in ALLOWED_SPECTRAL_ORDERS:
                 order=order,
                 filters=[al_thin, al_oxide],
                 reference_pixel=(slot_ref_pix+(0,))*u.pix,
-                design=short_as_designed_design,
+                design=short_as_built_design,
                 aperture=slot)
     )
 
 # Build instrument configurations
-moxsi_cdr_filtergrams = InstrumentDesign(
+moxsi_built_filtergrams = InstrumentDesign(
     'cdr-filtergrams',
     [filtergram_1, filtergram_2, filtergram_3, filtergram_4]
 )
-moxsi_cdr_spectrogram_pinhole = InstrumentDesign('cdr-dispersed-pinhole', spectrograms_pinhole)
-moxsi_cdr_spectrogram_slot = InstrumentDesign('cdr-dispersed-slot', spectrograms_slot)
-moxsi_cdr = moxsi_cdr_filtergrams + moxsi_cdr_spectrogram_pinhole + moxsi_cdr_spectrogram_slot
+moxsi_built_spectrogram_pinhole = InstrumentDesign('cdr-dispersed-pinhole', spectrograms_pinhole)
+moxsi_built_spectrogram_slot = InstrumentDesign('cdr-dispersed-slot', spectrograms_slot)
+moxsi_built = moxsi_built_filtergrams + moxsi_built_spectrogram_pinhole + moxsi_built_spectrogram_slot
